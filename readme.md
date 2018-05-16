0e3d6df78e4e27acad183d66c26b4ba6b956d6af
km-tr <kumagai@bear.tokyo>
2018-05-16 19:25:56 +0900

    init

---
+++ readme.md
@@ -0,0 +1,15 @@
+# km-tr
+
+> km-tr CLI
+
+## Usage
+
+Install Node.js, then:
+
+```
+$ npx km-tr
+```
+
+## License
+
+MIT © [Sindre Sorhus](https://sindresorhus.com)
---
+++ src/ui.js
@@ -0,0 +1,54 @@
+'use strict'
+const path = require('path')
+const { h, Text } = require('ink')
+const SelectInput = require('ink-select-input')
+const opn = require('opn')
+const terminalImage = require('terminal-image')
+
+const open = url => opn(url, { wait: false })
+
+const handleSelect = item => {
+  if (item.url) {
+    open(item.url)
+  }
+
+  if (item.action) {
+    item.action()
+  }
+}
+
+const items = [
+  {
+    label: 'GitHub',
+    url: 'https://github.com/km-tr',
+  },
+  {
+    label: 'Blog',
+    url: 'https://www.kumagai.tokyo',
+  },
+  {
+    label: 'Facebook',
+    url: 'https://www.facebook.com/beer.with.sushi',
+  },
+  {
+    label: 'Sketchfab',
+    url: 'https://sketchfab.com/kmgi',
+  },
+  {
+    label: 'Quit',
+    action() {
+      process.exit()
+    },
+  },
+]
+
+module.exports = () => (
+  <div>
+    <br />
+    <div>
+      <Text>I’m a Web FrontEnd developer in Roppongi.</Text>
+    </div>
+    <br />
+    <SelectInput items={items} onSelect={handleSelect} />
+  </div>
+)
